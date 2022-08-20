21f26dbc4e69b362f334756b043d60c415613f87
enricomartins018 <enrico.martins018@gmail.com>
2022-08-20 05:24:44 -0300

    feat: Components adicionados e importados no app.

--- mod5_mock_react/src/App.jsx
+++ mod5_mock_react/src/App.jsx
@@ -1,11 +1,22 @@
 import Header from './components/Header/Header'
+import Section from './components/Section/Section'
+import Collection from './components/Collection/Collection'
+import Title from './components/Title/Title'
+import Share from './components/Share/Share'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Section />
+      <Title texto='Sua seleção especial'/>
+      <Collection/>
+      <Title texto='Compartilhe a novidade'/>
+      <Share/>
+      <Footer/>
     </div>
   )
 }
