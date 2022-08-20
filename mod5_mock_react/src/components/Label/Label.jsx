7acc42444c0feed93568d8db58e6fcafec8a4467
enricomartins018 <enrico.martins018@gmail.com>
2022-08-20 05:23:25 -0300

    feat: Label finalizado.

---
+++ mod5_mock_react/src/components/Label/Label.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import S from '../Label/Label.module.css'
+
+const Label = ({texto}) => {
+  return (
+    <label className={S.labelPattern}>{texto}</label>
+  )
+}
+
+export default Label
