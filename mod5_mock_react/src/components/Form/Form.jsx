c75ef3a9f56cfd8141a2290ac12960be904b7d82
enricomartins018 <enrico.martins018@gmail.com>
2022-08-20 05:19:59 -0300

    feat: Form finalizado.

--- mod5_mock_react/src/components/Form/Form.jsx
+++ mod5_mock_react/src/components/Form/Form.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import S from '../Form/Form.module.css'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form className={S.formPattern}> 
+        <label className={S.labelForm}>Seu nome:</label>
+        <input className={S.inputForm} type="text" name='name' />
+        <label className={S.labelForm}>E-mail</label>
+        <input className={S.inputForm} type="email" name='email' />
+        <label className={S.labelForm}>CPF:</label>
+        <input className={S.inputForm} type="cpf" name='cpf' />
+        <div className={S.optionsSexual}>
+            <input className={S.inputsSexual} type="radio" />
+            <label>Masculino:</label>
+            <input type="radio" />
+            <label>Feminino:</label>
+        </div> 
+        <button className={S.btnForm} type='submit'>Enviar</button>
+    </form>
   )
 }
 
