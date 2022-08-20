0c311db9cafb0eaf3476f38bfd35eb1d01dfa97b
enricomartins018 <enrico.martins018@gmail.com>
2022-08-20 05:23:49 -0300

    feat: Share finalizada.

---
+++ mod5_mock_react/src/components/Share/Share.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import S from '../Share/Share.module.css'
+
+const Share = () => {
+    return (
+        <section className={S.sectionShare}>
+            <p>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
+            <div className={S.containerNameAndEmail}>
+                <div className={S.containerNameFriend}>
+                    <label>Nome do seu amigo:</label>
+                    <input className={S.inputNameFriend} type="name" />
+                </div>
+                <div className={S.containerEmailFriend}>
+                    <label>E-mail:</label>
+                    <input className={S.inputEmailFriend} type="email" />
+                </div>
+            </div>
+            <button className={S.btnSubmitShare} type="submit">Enviar agora</button>
+        </section>
+    )
+}
+
+export default Share
