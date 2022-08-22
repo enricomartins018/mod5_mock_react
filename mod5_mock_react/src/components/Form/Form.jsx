98981450285051d3cddad84744a677781fbb3820
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 05:59:06 -0300

    feat: Componente Form finalizado.

--- mod5_mock_react/src/components/Form/Form.jsx
+++ mod5_mock_react/src/components/Form/Form.jsx
@@ -3,20 +3,20 @@ import S from '../Form/Form.module.css'
 
 const Form = () => {
   return (
-    <form className={S.formPattern}> 
-        <label className={S.labelForm} htmlFor='name'>Seu nome:</label>
-        <input className={S.inputForm} type="text" name='name' id='name' />
-        <label className={S.labelForm} htmlFor='email'>E-mail </label>
-        <input className={S.inputForm} type="email" name='email' id='email' />
-        <label className={S.labelForm} htmlFor='cpf'>CPF:</label>
-        <input className={S.inputForm} type="text" name='cpf' id='cpf'/>
-        <div className={S.optionsSexual}>
-            <input className={S.inputsSexual} type="radio" name='sexualidade' checked/>
-            <label>Masculino</label>
-            <input type="radio" name='sexualidade' />
-            <label>Feminino</label>
-        </div> 
-        <button className={S.btnForm} type='submit'>Enviar</button>
+    <form className={S.formPattern}>
+      <label className={S.labelForm} htmlFor='name'>Seu nome:</label>
+      <input className={S.inputForm} type="text" name='name' id='name' />
+      <label className={S.labelForm} htmlFor='email'>E-mail </label>
+      <input className={S.inputForm} type="email" name='email' id='email' />
+      <label className={S.labelForm} htmlFor='cpf'>CPF:</label>
+      <input className={S.inputForm} type="text" name='cpf' id='cpf' />
+      <div className={S.optionsSexual}>
+        <input className={S.inputsSexual} type="radio" name='sexualidade' checked />
+        <label>Masculino</label>
+        <input type="radio" name='sexualidade' />
+        <label>Feminino</label>
+      </div>
+      <button className={S.btnForm} type='submit'>Enviar</button>
     </form>
   )
 }
