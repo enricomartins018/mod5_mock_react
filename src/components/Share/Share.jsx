d7e9510a3083ab575fdc3fb3f5632f997d03d80b
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 06:32:07 -0300

    feat: Share formatado.

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import S from '../Share/Share.module.css'
+import React from "react";
+import S from "../Share/Share.module.css";
 
 const Share = () => {
-    return (
-        <section className={S.sectionShare}>
+  return (
+    <section className={S.sectionShare}>
+      <p>
+        Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+        agora!
+      </p>
+      <div className={S.containerNameAndEmail}>
+        <div className={S.containerNameFriend}>
+          <label htmlFor="nameShare">Nome do seu amigo:</label>
+          <input
+            className={S.inputNameFriend}
+            type="text"
+            id="nameShare"
+            name="name"
+          />
+        </div>
+        <div className={S.containerEmailFriend}>
+          <label htmlFor="emailShare">E-mail:</label>
+          <input
+            className={S.inputEmailFriend}
+            type="email"
+            id="emailShare"
+            name="email"
+          />
+        </div>
+      </div>
+      <button className={S.btnSubmitShare} type="submit">
+        Enviar agora
+      </button>
+    </section>
+  );
+};
 
-            <p>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
-            <div className={S.containerNameAndEmail}>
-                <div className={S.containerNameFriend}>
-                    <label htmlFor='nameShare'>Nome do seu amigo:</label>
-                    <input className={S.inputNameFriend} type="text" id='nameShare' name='name' />
-                </div>
-                <div className={S.containerEmailFriend}>
-                    <label htmlFor='emailShare'>E-mail:</label>
-                    <input className={S.inputEmailFriend} type="email" id='emailShare' name='email' />
-                </div>
-            </div>
-            <button className={S.btnSubmitShare} type="submit">Enviar agora</button>
-            
-        </section>
-    )
-}
-
-export default Share
+export default Share;
