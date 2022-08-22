670acc06c5a47484a1220bb218d99ecc6b1936f1
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 06:31:07 -0300

    feat: Footer formatado.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import S from '../Footer/Footer.module.css'
+import React from "react";
+import S from "../Footer/Footer.module.css";
 const Footer = () => {
-    return (
-        <footer className={S.footerIndex}>
-            <div className={S.msgFooter}>
-                <p>Testando suas habilidades em HTML, CSS e JS.</p>
-                <p>Linx Impulse</p>
-                <p>2019</p>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className={S.footerIndex}>
+      <div className={S.msgFooter}>
+        <p>Testando suas habilidades em HTML, CSS e JS.</p>
+        <p>Linx Impulse</p>
+        <p>2019</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
