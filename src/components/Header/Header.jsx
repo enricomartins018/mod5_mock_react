1c69a5d67b8f648de64b4b46130edb9cd72f6ed4
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 06:31:29 -0300

    feat: Header formatado.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import S from '../Header/Header.module.css'
-import Menu from '../Menu/Menu'
+import React from "react";
+import S from "../Header/Header.module.css";
+import Menu from "../Menu/Menu";
 
 const Header = () => {
   return (
@@ -8,11 +8,14 @@ const Header = () => {
       <div className={S.containerHeader}>
         <h2 className={S.subTitulo}>uma seleção de produtos</h2>
         <h1 className={S.titulo}>especial para você</h1>
-        <p className={S.paragrafo}>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+        <p className={S.paragrafo}>
+          Todos os produtos desta lista foram selecionados a partir da sua
+          navegação. Aproveite!
+        </p>
         <Menu />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
