948a6d41c8b4c14dc1ff5e66f00fcd7ff078d9f8
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 06:31:50 -0300

    feat: Menu formatado.

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Button from '../Button/Button'
-import S from '../Menu/Menu.module.css'
+import React from "react";
+import Button from "../Button/Button";
+import S from "../Menu/Menu.module.css";
 
 const Menu = () => {
-    return (
-        <div className={S.menuContainer}>
-            <Button texto='Conheça a Linx' />
-            <Button texto='Ajude o algoritmo' />
-            <Button texto='Seus produtos' />
-            <Button texto='Compartilhe' />
-        </div>
-    )
-}
+  return (
+    <div className={S.menuContainer}>
+      <Button texto="Conheça a Linx" />
+      <Button texto="Ajude o algoritmo" />
+      <Button texto="Seus produtos" />
+      <Button texto="Compartilhe" />
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
