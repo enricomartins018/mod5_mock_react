f43f110397f78118d804138fc4706fb07a9df4d2
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 05:59:25 -0300

    feat: Componente Label finalizado.

--- mod5_mock_react/src/components/Label/Label.jsx
+++ mod5_mock_react/src/components/Label/Label.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import S from '../Label/Label.module.css'
 
-const Label = ({texto}) => {
+const Label = ({ texto }) => {
   return (
     <label className={S.labelPattern}>{texto}</label>
   )
