0512a5b90754786a8557cff8fe1248042db42c0f
enricomartins018 <enrico.martins018@gmail.com>
2022-08-22 06:32:22 -0300

    feat: Title formatado.

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import S from '../Title/Title.module.css'
+import React from "react";
+import S from "../Title/Title.module.css";
 
 const Titulo = ({ texto }) => {
   return (
     <h1 className={S.titleIndex}>
-
       <span>{texto}</span>
-      
     </h1>
   );
 };
